bca2b5397c7d1bf73c239a6182205464a72bdd12
shashimi00 <60232711+shashimi00@users.noreply.github.com>
2020-03-17 16:44:31 -0700

    Delete test_CSOR.py

--- test/test_CSOR.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from CSOR import CSOR
-
-
-class TestCSOR(TestCase):
-    def setUp(self):
-        self.Alif = CSOR('1AF', 32, 'A77792859', 'Pte', 'Billah', 'Alif', 200, 600)
